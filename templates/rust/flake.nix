c6bf7fb70860ffe4a057edb98b2404285da3187f
Lautaro Acosta Quintana <me@lautaroacosta.com>
2026-07-09 14:12:47 -0300

    fixed an error in the template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -28,6 +28,13 @@
   }:
     flake-parts.lib.mkFlake { inherit inputs; } {
 
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
       imports = with tanko.flakeModules; [
         base
         rust
