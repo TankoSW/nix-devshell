f9684301b8a04229fa625ca5680eed3acbb6e5bb
Lautaro Acosta Quintana <me@lautaroacosta.com>
2026-09-15 20:22:22 -0300

    feat: added llvm-tools to the shell

--- modules/rust.nix
+++ modules/rust.nix
@@ -3,21 +3,27 @@
 {
   perSystem = { pkgs, ... }: {
     devShells.rust = pkgs.mkShell {
-      packages = with pkgs; [
-        rustc
-        cargo
-        rustfmt
-        clippy
-        rust-analyzer
-        pkg-config
-      ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-        pkgs.libiconv
-      ];
+      packages =
+        with pkgs;
+        [
+          rustc
+          cargo
+          rustfmt
+          clippy
+          rust-analyzer
+          llvmPackages.llvm
+          pkg-config
+        ]
+        ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+        ];
 
       RUST_BACKTRACE = "1";
 
       shellHook = ''
         export CARGO_HOME="$PWD/.cargo"
+        export LLVM_COV="${pkgs.llvmPackages.llvm}/bin/llvm-cov"
+        export LLVM_PROFDATA="${pkgs.llvmPackages.llvm}/bin/llvm-profdata"
       '';
     };
   };
